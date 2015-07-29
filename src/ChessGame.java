6b7c26fca9ccc8ca8cb0a90610d8038ac85d16e2
Shravan Venkataraman <heraclesphaeton@gmail.com>
2015-07-29 12:13:31 +0530

    Created Input File Parser function

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -1,4 +1,18 @@
+import java.io.*;
+import java.util.ArrayList;
 
 public class ChessGame {
 
+	public static ArrayList<String> parseInput(BufferedReader br)
+	{
+		ArrayList<String> parsedInput = new ArrayList<String> ();
+		return parsedInput;
+	}
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		File file = new File("C:/Users/test/Downloads/Bootcamp/Programs/Akopian.pgn");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		ArrayList<String> positionsInput = new ArrayList<String> ();
+		
+	}
 }
