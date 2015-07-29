5d783cb59fbad2b76663c15587dcc240d4d216d4
Shravan Venkataraman <heraclesphaeton@gmail.com>
2015-07-29 14:28:10 +0530

    Implemented equivalentBoardPosition function

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -29,6 +29,22 @@ public class ChessBoard {
     public static String getEquivalentBoardPosition(String position)
     {
     	
+    	if(position.charAt(0) == 'A')
+    		return "1"+","+position.charAt(1);
+    	if(position.charAt(0) == 'B')
+    		return "2"+","+position.charAt(1);
+    	if(position.charAt(0) == 'C')
+    		return "3"+","+position.charAt(1);
+    	if(position.charAt(0) == 'D')
+    		return "4"+","+position.charAt(1);
+    	if(position.charAt(0) == 'E')
+    		return "5"+","+position.charAt(1);
+    	if(position.charAt(0) == 'F')
+    		return "6"+","+position.charAt(1);
+    	if(position.charAt(0) == 'G')
+    		return "7"+","+position.charAt(1);
+    	if(position.charAt(0) == 'H')
+    		return "8"+","+position.charAt(1);
     }
 }
 
