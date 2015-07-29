d62abaabc8bd093be58a87b9ec73d7cb62ca797e
Asha Jyothi <ashajyothi.pryr@gmail.com>
2015-07-29 12:29:45 +0530

    Created ChessBoard.java

---
+++ src/ChessBoard.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class ChessBoard {
+	private String initialState[] = new String[8];
+	string position;
+
+	public ChessBoard( {
+		String[8] = {"RNBQKBNR","PPPPPPPP", "--------", "--------", "--------", "--------", "pppppppp", "rnbqkbnr"};
+		this.position=" ";
+		      }
+
+
+	public getPosition(){
+	return position;
+		}
+		
+}
+
+
